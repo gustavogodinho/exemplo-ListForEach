290a3e298e524d7bd8702fd558f859e93e88ad3b
Gustavo Gilberto Godinho <gustavo.godinho@ituran.com.br>
2019-10-09 18:02:24 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExemploForEach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Funcionario> names = new List<Funcionario>();
+
+            names.Add(new Funcionario() {ID = 1,Nome = "Bruce"});
+            names.Add(new Funcionario() {ID = 1,Nome = "Alfred"});
+            names.Add(new Funcionario() {ID = 1,Nome = "Tim"});
+            names.Add(new Funcionario() {ID = 1,Nome = "Richard"});
+            names.Add(new Funcionario() {ID = 1,Nome = "Gustavo"});
+      
+    
+        names.ForEach(delegate(Funcionario func)
+        {
+          System.Console.WriteLine(func);
+        });
+
+        names.ForEach((s)=> {
+            s.Nome = "Hey Ho!";
+            System.Console.WriteLine(s);
+        });
+        }
+
+        public class Funcionario 
+        {
+            public int ID { get; set; }
+            public string Nome { get; set; }
+
+        public override string ToString()
+        {
+            return "Id: " + ID + " Nome: " + Nome;
+        }
+
+        }
+
+          
+    }
+}
